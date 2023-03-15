13e7a84105d68a2cc7020b988ae86d7b195f3c76
Ben Murray <benmurray573@gmail.com>
2023-03-15 12:14:33 +0000

    new comp Characters with interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Characters from "./components/Characters";
+
 function App(): JSX.Element {
-  return <h1>Hello World!</h1>;
+  return(
+    <>
+    <h1>Hello World!</h1>
+    <Characters />
+    </>
+  ) 
 }
 
 export default App;
---
+++ src/components/Characters.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+interface CharData {
+    id: string;
+    name: string;
+    alternate_names: string[];
+    species: string;
+    gender: string;
+    house: string;
+    dateOfBirth: string;
+    yearOfBirth: string;
+    wizard: boolean;
+    ancestry: string;
+    eyeColour: string;
+    hairColour: string;
+    wand: {"wood": string, "core": string, "length": string};
+    patronus: string;
+    hogwartsStudent: boolean;
+    hogwartsStaff: boolean;
+    actor: string;
+    alternate_actors: string[];
+    alive: boolean;
+    image: string;
+}
+
+function Characters(): JSX.Element {
+    const [characters, setCharacters] = useState<CharData[]>([])
+    return (
+        <>
+            <div>
+                <p>Character data</p>
+            </div>
+        </>
+    )
+}
+
+export default Characters
