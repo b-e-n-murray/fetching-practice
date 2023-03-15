0c875a9e90db4d5b8d662515c6a6a3b759241d2a
Ben Murray <benmurray573@gmail.com>
2023-03-15 12:14:56 +0000

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Characters from "./components/Characters";
 
 function App(): JSX.Element {
-  return(
+  return (
     <>
-    <h1>Hello World!</h1>
-    <Characters />
+      <h1>Hello World!</h1>
+      <Characters />
     </>
-  ) 
+  );
 }
 
 export default App;
--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,37 +1,37 @@
-import { useState } from "react"
+import { useState } from "react";
 
 interface CharData {
-    id: string;
-    name: string;
-    alternate_names: string[];
-    species: string;
-    gender: string;
-    house: string;
-    dateOfBirth: string;
-    yearOfBirth: string;
-    wizard: boolean;
-    ancestry: string;
-    eyeColour: string;
-    hairColour: string;
-    wand: {"wood": string, "core": string, "length": string};
-    patronus: string;
-    hogwartsStudent: boolean;
-    hogwartsStaff: boolean;
-    actor: string;
-    alternate_actors: string[];
-    alive: boolean;
-    image: string;
+  id: string;
+  name: string;
+  alternate_names: string[];
+  species: string;
+  gender: string;
+  house: string;
+  dateOfBirth: string;
+  yearOfBirth: string;
+  wizard: boolean;
+  ancestry: string;
+  eyeColour: string;
+  hairColour: string;
+  wand: { wood: string; core: string; length: string };
+  patronus: string;
+  hogwartsStudent: boolean;
+  hogwartsStaff: boolean;
+  actor: string;
+  alternate_actors: string[];
+  alive: boolean;
+  image: string;
 }
 
 function Characters(): JSX.Element {
-    const [characters, setCharacters] = useState<CharData[]>([])
-    return (
-        <>
-            <div>
-                <p>Character data</p>
-            </div>
-        </>
-    )
+  const [characters, setCharacters] = useState<CharData[]>([]);
+  return (
+    <>
+      <div>
+        <p>Character data</p>
+      </div>
+    </>
+  );
 }
 
-export default Characters
+export default Characters;
